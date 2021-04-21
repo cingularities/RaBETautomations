42a2e9a0c79101b0778678b3f2247291f39046c2
CLNorton <67613055+cingularities@users.noreply.github.com>
2021-04-21 10:38:32 -0700

    Update model_MLRA.R

--- R/model_MLRA.R
+++ R/model_MLRA.R
@@ -5,9 +5,9 @@
 #' @examples
 #' model_MLRA(rawdata)
 
-model_MLRA <- function(raw_data) { #filter list and raw data files
-  
-  group_MLRA <- raw_data %>% #created a grouped data frame from the tbl_df
+model_MLRA <- function(raw_data) { #filter list and raw data files and runs linear models on all all 2 samples combination dataset
+
+  group_MLRA <- raw_data %>% #creates a grouped data frame from the tbl_df
     group_by(HI_RES_ID)
   
   split_MLRA <- group_split(group_MLRA) #split grouped_df by group
@@ -21,49 +21,46 @@ model_MLRA <- function(raw_data) { #filter list and raw data files
   }
   
   combination_MLRA <- as.data.frame(lapply(class, get_combn)) %>% #applying get_combo function to class list
-    separate(HI_RES_ID, c("A", "B")) #seperates
+  separate(HI_RES_ID, c("A", "B")) #seperates
   
-  
-  mae_list <- list()
-  coefficients_list <- list()
-  
-  for (i in 1:nrow(combination_MLRA)) {
-    # Extract values from B column for this row
-    A_class <- combination_MLRA$A[i]
-    B_class <- combination_MLRA$B[i]
-    # Pull out data from df_2 where the value in the HI_RES_ID column matches the 
-    # value from df_1
-    A_subset <- raw_data[raw_data$HI_RES_ID == A_class, ]
-    B_subset <- raw_data[raw_data$HI_RES_ID == B_class, ]
-    # Do linear regression for that subset of data
-    filtered_MLRA <- rbind(A_subset,B_subset)
+  mae <- function(error){ #function for mean absolute error
+    mean(abs(error))
+  }
+
+  mae_list <- list() #creates empty list for mae dataset
+  coefficients_list <- list() #creates empty list for model coefficients 
+
+  for (i in 1:nrow(combination_MLRA)) { #loop through combination list 
+    # Extract values from from column 
+    A_class <- combination_MLRA$A[i] #assign value of sample 1 to class var
+    B_class <- combination_MLRA$B[i] #assign value of sample 2 to class var
     
-    linear_model <- lm(NAIP_WC ~ WVI + MSAVI + NDI5, data = filtered_MLRA)
-    model_summary <- tidy(linear_model)
-    model_coefficients<- coef(summary(linear_model), data=raw_data)
-    coefficients_list[[i]] <- model_coefficients
+    # Pull out data from  where the value in the HI_RES_ID column matches the 
+    # Subset raw_data with selected combination_MLRA class
+    A_subset <- raw_data[raw_data$HI_RES_ID == A_class, ] #subset sample 1
+    B_subset <- raw_data[raw_data$HI_RES_ID == B_class, ] #subset sample 2
+        # Do linear regression for that subset of data
+    filtered_MLRA <- rbind(A_subset,B_subset) #bind filtered subsets
     
-    
+    linear_model <- lm(NAIP_WC ~ WVI + MSAVI + NDI5, data = filtered_MLRA) #run linear model
+    model_coefficients<- coef(summary(linear_model), data=raw_data) #extract model coefficients
+    coefficients_list[[i]] <- model_coefficients #input coefficient to list
     
     # Pull out the r.squared (as example)
-    predict_MLRA <- add_predictions(raw_data, linear_model)
-    mae <- function(error)
-    {
-      mean(abs(error))
-    }
-    error <- predict_MLRA$NAIP_WC - predict_MLRA$pred
-    MAE <- mae(error)
-    mae_list[[i]] <- MAE
+    predict_MLRA <- add_predictions(raw_data, linear_model) #predict raw based on linear model
+   
+    error <- predict_MLRA$NAIP_WC - predict_MLRA$pred #calculate error
+    MAE <- mae(error) #calculate MAE from funtion created
+    mae_list[[i]] <- MAE #add mae values to list
   }
-  
-  MAE_df <- combination_MLRA %>% add_column(MAE = mae_list)
-  MAE_final <- data.frame(lapply(MAE_df, as.character), stringsAsFactors=FALSE)
-  coefficients_df <- data.frame(matrix(unlist(coefficients_list), nrow=length(coefficients_list), byrow=TRUE))
-  coefficients_final <- coefficients_df %>% select(X1,X2,X3,X4,X13,X14,X15,X16) %>% 
+
+  MAE_df <- combination_MLRA %>% add_column(MAE = mae_list) #add MAE values to combination list
+  MAE_final <- data.frame(lapply(MAE_df, as.character), stringsAsFactors=FALSE) #creates the right format for export MAE and sample combination
+  coefficients_df <- data.frame(matrix(unlist(coefficients_list), nrow=length(coefficients_list), byrow=TRUE)) #creates the right format for export lm coefficients
+  coefficients_final <- coefficients_df %>% select(X1,X2,X3,X4,X13,X14,X15,X16) %>% #select wanted coefficients and renaming columns
     rename(est.int = X1, est.WVI = X2, est.MSAVI = X3, est.NDI5 = X4,
-           p.int = X13, p.WVI = X14, p.MSAVI = X15, p.NDI5 = X16)
+          p.int = X13, p.WVI = X14, p.MSAVI = X15, p.NDI5 = X16)
   
-  final_file <- cbind(MAE_final,coefficients_final)
-  #return(MAE_final)
-  return(final_file)
+  final_file <- cbind(MAE_final,coefficients_final) #binds MAE, sample combination and wanted coefficients
+  return(final_file) #return final_file
 }
